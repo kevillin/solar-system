b05f9b57b7c3ef2910a00c509e7797b996e49090
Kevillin Santos <kevillin_santos@hotmail.com>
2023-03-18 12:42:53 -0300

    feat: renderiza os planetas por meio de um map do PlanetCard no componente SolarSystem

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 import Title from './Title';
 
 function SolarSystem() {
   return (
     <div data-testid="solar-system">
       <Title headline="Planetas" />
+      {planets.map((planet) => (
+        <PlanetCard
+          key={ planet.name }
+          planetName={ planet.name }
+          planetImage={ planet.image }
+        />
+      ))}
     </div>
   );
 }
